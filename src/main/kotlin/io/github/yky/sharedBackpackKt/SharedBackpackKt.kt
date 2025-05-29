46ed70ec61e9cd1e2bc3d9ca90e4df91ac32d18b
yukiyosanako <179217493+T2PeNBiX99wcoxKv3A4g@users.noreply.github.com>
2025-05-29 21:06:58 +0800

    fix(TrashCommand): Stop register argument type `TrashTypeArgument`

--- src/main/kotlin/io/github/yky/sharedBackpackKt/SharedBackpackKt.kt
+++ src/main/kotlin/io/github/yky/sharedBackpackKt/SharedBackpackKt.kt
@@ -2,30 +2,25 @@ package io.github.yky.sharedBackpackKt
 
 import com.mojang.brigadier.CommandDispatcher
 import io.github.yky.sharedBackpackKt.Utils.Logger
-import io.github.yky.sharedBackpackKt.Utils.MOD_ID
-import io.github.yky.sharedBackpackKt.argument.TrashTypeArgument
 import io.github.yky.sharedBackpackKt.command.BackpackCommand
 import io.github.yky.sharedBackpackKt.command.BackpackPlayerOnlyCommand
 import io.github.yky.sharedBackpackKt.command.TrashCommand
 import net.fabricmc.api.ModInitializer
-import net.fabricmc.fabric.api.command.v2.ArgumentTypeRegistry
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents.ServerStarted
 import net.minecraft.command.CommandRegistryAccess
-import net.minecraft.command.argument.serialize.ConstantArgumentSerializer
 import net.minecraft.server.MinecraftServer
 import net.minecraft.server.command.CommandManager
 import net.minecraft.server.command.ServerCommandSource
-import net.minecraft.util.Identifier
 
 class SharedBackpackKt : ModInitializer {
     override fun onInitialize() {
-        ArgumentTypeRegistry.registerArgumentType(
-            Identifier.of(MOD_ID, TrashTypeArgument.ID),
-            TrashTypeArgument::class.java,
-            ConstantArgumentSerializer.of(::TrashTypeArgument)
-        )
+//        ArgumentTypeRegistry.registerArgumentType(
+//            Identifier.of(MOD_ID, TrashTypeArgument.ID),
+//            TrashTypeArgument::class.java,
+//            ConstantArgumentSerializer.of(::TrashTypeArgument)
+//        )
 
         CommandRegistrationCallback.EVENT.register { commandDispatcher: CommandDispatcher<ServerCommandSource>, _: CommandRegistryAccess, _: CommandManager.RegistrationEnvironment ->
             BackpackCommand.register(commandDispatcher)
