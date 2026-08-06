0e3c86c362d306aa6f9b6201618ecc3046a46e14
yukiyosanako <179217493+T2PeNBiX99wcoxKv3A4g@users.noreply.github.com>
2026-08-06 15:20:53 +0800

    fix: update `checkTakeAchievements` method to reflect Level parameter change

--- src/main/java/io/github/yky/sharedBackpackKt/mixin/FurnaceResultSlotMixin.java
+++ src/main/java/io/github/yky/sharedBackpackKt/mixin/FurnaceResultSlotMixin.java
@@ -9,6 +9,7 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.FurnaceResultSlot;
 import net.minecraft.world.inventory.Slot;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 
@@ -18,9 +19,9 @@ abstract class FurnaceResultSlotMixin extends Slot {
         super(container, i, j, k);
     }
 
-    @WrapOperation(method = "checkTakeAchievements", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/ItemStack;onCraftedBy(Lnet/minecraft/world/entity/player/Player;I)V"))
-    private void checkTakeAchievements(ItemStack instance, Player player, int i, Operation<Void> original) {
-        original.call(instance, player, i);
+    @WrapOperation(method = "checkTakeAchievements", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/ItemStack;onCraftedBy(Lnet/minecraft/world/level/Level;Lnet/minecraft/world/entity/player/Player;I)V"))
+    private void checkTakeAchievements(ItemStack instance, Level level, Player player, int i, Operation<Void> original) {
+        original.call(instance, level, player, i);
         if (player instanceof ServerPlayer serverPlayer && container instanceof AbstractFurnaceContainer furnaceContainer)
             furnaceContainer.awardUsedRecipesAndPopExperience(serverPlayer);
     }
