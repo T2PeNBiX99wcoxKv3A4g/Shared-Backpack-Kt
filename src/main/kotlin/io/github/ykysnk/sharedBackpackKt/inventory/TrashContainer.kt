693d42e0fe23bb1e7c6f8f0378562b6d64a5d325
yukiyosanako <179217493+T2PeNBiX99wcoxKv3A4g@users.noreply.github.com>
2026-08-11 09:08:07 +0800

    fix(inventory): prevent `stopOpen` from closing non-empty trash containers and refactor `clearContent` handling

--- src/main/kotlin/io/github/ykysnk/sharedBackpackKt/inventory/TrashContainer.kt
+++ src/main/kotlin/io/github/ykysnk/sharedBackpackKt/inventory/TrashContainer.kt
@@ -18,6 +18,7 @@ class TrashContainer(val onStopOpen: (Player) -> Unit) : SimpleContainer(54) {
     }
 
     override fun stopOpen(player: Player) {
+        if (!isEmpty) return
         onStopOpen(player)
     }
 }
