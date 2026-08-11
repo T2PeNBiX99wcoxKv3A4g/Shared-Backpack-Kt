2b4454d93896e3385ca1e8440122ccafae3673f8
yukiyosanako <179217493+T2PeNBiX99wcoxKv3A4g@users.noreply.github.com>
2026-08-12 00:29:55 +0800

    refactor(inventory): remove redundant `getBurnDuration` overrides from furnace containers

--- src/main/kotlin/io/github/ykysnk/sharedBackpackKt/inventory/UnlimitedBlastFurnaceContainer.kt
+++ src/main/kotlin/io/github/ykysnk/sharedBackpackKt/inventory/UnlimitedBlastFurnaceContainer.kt
@@ -1,11 +1,6 @@
 package io.github.ykysnk.sharedBackpackKt.inventory
 
-import net.minecraft.world.item.ItemStack
 import net.minecraft.world.item.crafting.RecipeType
 
 class UnlimitedBlastFurnaceContainer(name: String, override val onNoPlayersOpen: () -> Unit) :
-    AbstractUnlimitedFurnaceContainer("shared-unlimited-furnace-blasting-${name}", RecipeType.BLASTING) {
-    override fun getBurnDuration(stack: ItemStack): Int {
-        return super.getBurnDuration(stack) / 2
-    }
-}
+    AbstractUnlimitedFurnaceContainer("shared-unlimited-furnace-blasting-${name}", RecipeType.BLASTING)
--- src/main/kotlin/io/github/ykysnk/sharedBackpackKt/inventory/UnlimitedSmokerContainer.kt
+++ src/main/kotlin/io/github/ykysnk/sharedBackpackKt/inventory/UnlimitedSmokerContainer.kt
@@ -1,11 +1,6 @@
 package io.github.ykysnk.sharedBackpackKt.inventory
 
-import net.minecraft.world.item.ItemStack
 import net.minecraft.world.item.crafting.RecipeType
 
 class UnlimitedSmokerContainer(name: String, override val onNoPlayersOpen: () -> Unit) :
-    AbstractUnlimitedFurnaceContainer("shared-unlimited-furnace-smoking-${name}", RecipeType.SMOKING) {
-    override fun getBurnDuration(stack: ItemStack): Int {
-        return super.getBurnDuration(stack) / 2
-    }
-}
+    AbstractUnlimitedFurnaceContainer("shared-unlimited-furnace-smoking-${name}", RecipeType.SMOKING)
