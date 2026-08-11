8fe26e5866c44dea39840502e3857843e2961b64
yukiyosanako <179217493+T2PeNBiX99wcoxKv3A4g@users.noreply.github.com>
2026-08-11 08:46:33 +0800

    refactor(utils): extract container caching logic into `ContainerManager` for better modularity and clarity

--- src/main/kotlin/io/github/ykysnk/sharedBackpackKt/Utils.kt
+++ src/main/kotlin/io/github/ykysnk/sharedBackpackKt/Utils.kt
@@ -1,15 +1,18 @@
 package io.github.ykysnk.sharedBackpackKt
 
-import io.github.ykysnk.sharedBackpackKt.inventory.*
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents
 import net.fabricmc.loader.api.FabricLoader
 import net.minecraft.server.MinecraftServer
-import net.minecraft.world.entity.player.Player
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import java.nio.file.Path
 import kotlin.io.path.createDirectories
 
 object Utils {
+    init {
+        ServerLifecycleEvents.SERVER_STARTED.register { Server = it }
+    }
+
     const val MOD_ID = "shared-backpack-kt"
     const val MOD_NAME = "Shared Backpack Kotlin"
     const val COMMAND_FAILED_PLAYER_ONLY = "This command can only be used by players."
@@ -19,98 +22,5 @@ object Utils {
         val dir = FabricLoader.getInstance().configDir.resolve(MOD_ID)
         dir.createDirectories()
         dir
-    }
-
-    private val BackpackContainerCache: MutableMap<String, BackpackContainer> = mutableMapOf()
-    private val BackpackPlayerOnlyContainerCache: MutableMap<String, BackpackPlayerOnlyContainer> = mutableMapOf()
-    private val TrashContainerCache: MutableMap<String, TrashContainer> = mutableMapOf()
-    private val FurnaceContainerCache: MutableMap<String, FurnaceContainer> = mutableMapOf()
-    private val FurnacePlayerOnlyContainerCache: MutableMap<String, FurnacePlayerOnlyContainer> = mutableMapOf()
-    private val BlastFurnaceContainerCache: MutableMap<String, BlastFurnaceContainer> = mutableMapOf()
-    private val BlastFurnacePlayerOnlyContainerCache: MutableMap<String, BlastFurnacePlayerOnlyContainer> =
-        mutableMapOf()
-    private val SmokerContainerCache: MutableMap<String, SmokerContainer> = mutableMapOf()
-    private val SmokerPlayerOnlyContainerCache: MutableMap<String, SmokerPlayerOnlyContainer> = mutableMapOf()
-
-    fun getOrCreateBackpackContainer(name: String): BackpackContainer {
-        if (BackpackContainerCache.containsKey(name)) return BackpackContainerCache[name]!!
-        backupBackpackData()
-        val container = BackpackContainer(name)
-        BackpackContainerCache[name] = container
-        return container
-    }
-
-    fun getOrCreateBackpackPlayerOnlyContainer(player: Player, name: String): BackpackPlayerOnlyContainer {
-        val id = "${player.stringUUID}-${name}"
-        if (BackpackPlayerOnlyContainerCache.containsKey(id)) return BackpackPlayerOnlyContainerCache[id]!!
-        backupBackpackData()
-        val container = BackpackPlayerOnlyContainer(player, name)
-        BackpackPlayerOnlyContainerCache[id] = container
-        return container
-    }
-
-    fun getOrCreateTrashContainer(player: Player): TrashContainer {
-        if (TrashContainerCache.containsKey(player.stringUUID)) return TrashContainerCache[player.stringUUID]!!
-        val container = TrashContainer()
-        TrashContainerCache[player.stringUUID] = container
-        return container
-    }
-
-    fun getOrCreateNormalFurnaceContainer(player: Player, name: String): FurnaceContainer {
-        if (FurnaceContainerCache.containsKey(name)) return FurnaceContainerCache[name]!!
-        backupBackpackData()
-        val container = FurnaceContainer(player, name)
-        FurnaceContainerCache[name] = container
-        return container
-    }
-
-    fun getOrCreateNormalFurnacePlayerOnlyContainer(player: Player, name: String): FurnacePlayerOnlyContainer {
-        if (FurnacePlayerOnlyContainerCache.containsKey(name)) return FurnacePlayerOnlyContainerCache[name]!!
-        backupBackpackData()
-        val container = FurnacePlayerOnlyContainer(player, name)
-        FurnacePlayerOnlyContainerCache[name] = container
-        return container
-    }
-
-    fun getOrCreateBlastFurnaceContainer(player: Player, name: String): BlastFurnacePlayerOnlyContainer {
-        if (BlastFurnacePlayerOnlyContainerCache.containsKey(name)) return BlastFurnacePlayerOnlyContainerCache[name]!!
-        backupBackpackData()
-        val container = BlastFurnacePlayerOnlyContainer(player, name)
-        BlastFurnacePlayerOnlyContainerCache[name] = container
-        return container
-    }
-
-    fun getOrCreateBlastFurnacePlayerOnlyContainer(
-        player: Player, name: String
-    ): BlastFurnacePlayerOnlyContainer {
-        if (BlastFurnacePlayerOnlyContainerCache.containsKey(name)) return BlastFurnacePlayerOnlyContainerCache[name]!!
-        backupBackpackData()
-        val container = BlastFurnacePlayerOnlyContainer(player, name)
-        BlastFurnacePlayerOnlyContainerCache[name] = container
-        return container
-    }
-
-    fun getOrCreateSmokerFurnaceContainer(player: Player, name: String): SmokerContainer {
-        if (SmokerContainerCache.containsKey(name)) return SmokerContainerCache[name]!!
-        backupBackpackData()
-        val container = SmokerContainer(player, name)
-        SmokerContainerCache[name] = container
-        return container
-    }
-
-    fun getOrCreateSmokerFurnacePlayerOnlyContainer(player: Player, name: String): SmokerPlayerOnlyContainer {
-        if (SmokerPlayerOnlyContainerCache.containsKey(name)) return SmokerPlayerOnlyContainerCache[name]!!
-        backupBackpackData()
-        val container = SmokerPlayerOnlyContainer(player, name)
-        SmokerPlayerOnlyContainerCache[name] = container
-        return container
-    }
-
-    private fun backupBackpackData() {
-        for (cache in BackpackContainerCache.values) cache.backupBackpackData()
-        for (cache in BackpackPlayerOnlyContainerCache.values) cache.backupBackpackData()
-        for (cache in FurnaceContainerCache.values) cache.backupBackpackData()
-        for (cache in BlastFurnaceContainerCache.values) cache.backupBackpackData()
-        for (cache in SmokerContainerCache.values) cache.backupBackpackData()
     }
 }
---
+++ src/main/kotlin/io/github/ykysnk/sharedBackpackKt/inventory/ContainerManager.kt
@@ -0,0 +1,118 @@
+package io.github.ykysnk.sharedBackpackKt.inventory
+
+import io.github.ykysnk.sharedBackpackKt.Utils
+import io.github.ykysnk.sharedBackpackKt.Utils.Logger
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents
+import net.minecraft.world.entity.player.Player
+import java.nio.file.Files
+
+object ContainerManager {
+    init {
+        ServerLifecycleEvents.SERVER_STOPPING.register {
+            clear()
+            backupBackpackData()
+        }
+    }
+
+    private val BackpackContainerCache: MutableMap<String, BackpackContainer> = mutableMapOf()
+    private val BackpackPlayerOnlyContainerCache: MutableMap<String, BackpackPlayerOnlyContainer> = mutableMapOf()
+    private val TrashContainerCache: MutableMap<String, TrashContainer> = mutableMapOf()
+    private val FurnaceContainerCache: MutableMap<String, FurnaceContainer> = mutableMapOf()
+    private val FurnacePlayerOnlyContainerCache: MutableMap<String, FurnacePlayerOnlyContainer> = mutableMapOf()
+    private val BlastFurnaceContainerCache: MutableMap<String, BlastFurnaceContainer> = mutableMapOf()
+    private val BlastFurnacePlayerOnlyContainerCache: MutableMap<String, BlastFurnacePlayerOnlyContainer> =
+        mutableMapOf()
+    private val SmokerContainerCache: MutableMap<String, SmokerContainer> = mutableMapOf()
+    private val SmokerPlayerOnlyContainerCache: MutableMap<String, SmokerPlayerOnlyContainer> = mutableMapOf()
+
+    fun getOrCreateBackpackContainer(name: String) = BackpackContainerCache.getOrPut(name) {
+        BackpackContainer(name) {
+            BackpackContainerCache.remove(name)
+        }
+    }
+
+    fun getOrCreateBackpackPlayerOnlyContainer(player: Player, name: String): BackpackPlayerOnlyContainer {
+        val id = "${player.stringUUID}-${name}"
+        return BackpackPlayerOnlyContainerCache.getOrPut(id) {
+            BackpackPlayerOnlyContainer(player, name) { BackpackPlayerOnlyContainerCache.remove(id) }
+        }
+    }
+
+    fun getOrCreateTrashContainer(player: Player) =
+        TrashContainerCache.getOrPut(player.stringUUID) { TrashContainer { TrashContainerCache.remove(player.stringUUID) } }
+
+    fun getOrCreateNormalFurnaceContainer(player: Player, name: String) = FurnaceContainerCache.getOrPut(name) {
+        FurnaceContainer(player, name) { FurnaceContainerCache.remove(name) }
+    }
+
+    fun getOrCreateNormalFurnacePlayerOnlyContainer(player: Player, name: String): FurnacePlayerOnlyContainer {
+        val id = "${player.stringUUID}-${name}"
+        return FurnacePlayerOnlyContainerCache.getOrPut(id) {
+            FurnacePlayerOnlyContainer(player, name) { FurnacePlayerOnlyContainerCache.remove(id) }
+        }
+    }
+
+    fun getOrCreateBlastFurnaceContainer(player: Player, name: String) = BlastFurnaceContainerCache.getOrPut(name) {
+        BlastFurnaceContainer(player, name) { BlastFurnaceContainerCache.remove(name) }
+    }
+
+    fun getOrCreateBlastFurnacePlayerOnlyContainer(player: Player, name: String): BlastFurnacePlayerOnlyContainer {
+        val id = "${player.stringUUID}-${name}"
+        return BlastFurnacePlayerOnlyContainerCache.getOrPut(id) {
+            BlastFurnacePlayerOnlyContainer(player, name) { BlastFurnacePlayerOnlyContainerCache.remove(id) }
+        }
+    }
+
+    fun getOrCreateSmokerFurnaceContainer(player: Player, name: String) = SmokerContainerCache.getOrPut(name) {
+        SmokerContainer(player, name) { SmokerContainerCache.remove(name) }
+    }
+
+    fun getOrCreateSmokerFurnacePlayerOnlyContainer(player: Player, name: String): SmokerPlayerOnlyContainer {
+        val id = "${player.stringUUID}-${name}"
+        return SmokerPlayerOnlyContainerCache.getOrPut(id) {
+            SmokerPlayerOnlyContainer(player, name) { SmokerPlayerOnlyContainerCache.remove(id) }
+        }
+    }
+
+    fun clear() {
+        BackpackContainerCache.forEach { (_, container) -> container.saveNbt() }
+        BackpackContainerCache.clear()
+        BackpackPlayerOnlyContainerCache.forEach { (_, container) -> container.saveNbt() }
+        BackpackPlayerOnlyContainerCache.clear()
+        TrashContainerCache.clear()
+        FurnaceContainerCache.forEach { (_, container) -> container.saveNbt() }
+        FurnaceContainerCache.clear()
+        FurnacePlayerOnlyContainerCache.forEach { (_, container) -> container.saveNbt() }
+        FurnacePlayerOnlyContainerCache.clear()
+        BlastFurnaceContainerCache.forEach { (_, container) -> container.saveNbt() }
+        BlastFurnaceContainerCache.clear()
+        BlastFurnacePlayerOnlyContainerCache.forEach { (_, container) -> container.saveNbt() }
+        BlastFurnacePlayerOnlyContainerCache.clear()
+        SmokerContainerCache.forEach { (_, container) -> container.saveNbt() }
+        SmokerContainerCache.clear()
+        SmokerPlayerOnlyContainerCache.forEach { (_, container) -> container.saveNbt() }
+        SmokerPlayerOnlyContainerCache.clear()
+    }
+
+    private fun backupBackpackData() {
+        Files.list(Utils.ConfigDir).use { files ->
+            files
+                .filter { Files.isRegularFile(it) }
+                .filter { it.fileName.toString().endsWith(".dat") }
+                .forEach { path ->
+                    runCatching {
+                        val backupPath = path.resolveSibling("${path.fileName.toString().removeSuffix(".dat")}.dat_old")
+                        Files.deleteIfExists(backupPath)
+                        Files.copy(path, backupPath)
+                        Logger.info("Backed up backpack data to {}", backupPath)
+                    }.getOrElse {
+                        Logger.error(
+                            "Failed to back up backpack data: {}\n{}",
+                            it.localizedMessage,
+                            it.stackTraceToString()
+                        )
+                    }
+                }
+        }
+    }
+}
