de5d06e49e63c144b724eab7bd0d226b422848e0
yukiyosanako <179217493+T2PeNBiX99wcoxKv3A4g@users.noreply.github.com>
2026-08-11 09:28:31 +0800

    refactor(inventory): rename `onStopOpen` to `onEmpty` in `TrashContainer` for clarity

--- src/main/kotlin/io/github/ykysnk/sharedBackpackKt/inventory/TrashContainer.kt
+++ src/main/kotlin/io/github/ykysnk/sharedBackpackKt/inventory/TrashContainer.kt
@@ -6,7 +6,7 @@ import net.minecraft.world.SimpleContainer
 import net.minecraft.world.entity.player.Player
 import net.minecraft.world.item.ItemStack
 
-class TrashContainer(val onStopOpen: (Player) -> Unit) : SimpleContainer(54) {
+class TrashContainer(val onEmpty: (Player) -> Unit) : SimpleContainer(54) {
     override fun fromTag(listTag: ListTag, provider: HolderLookup.Provider) {
         for (i in 0..<containerSize)
             setItem(i, ItemStack.EMPTY)
@@ -19,6 +19,6 @@ class TrashContainer(val onStopOpen: (Player) -> Unit) : SimpleContainer(54) {
 
     override fun stopOpen(player: Player) {
         if (!isEmpty) return
-        onStopOpen(player)
+        onEmpty(player)
     }
 }
