1abfdd6a36485e607d5dbff266be5257511323fd
yukiyosanako <179217493+T2PeNBiX99wcoxKv3A4g@users.noreply.github.com>
2026-08-12 20:35:27 +0800

    feat(inventory): add `CustomFurnaceMenu` and enable access widener for `AbstractFurnaceMenu` customization

---
+++ src/main/kotlin/io/github/ykysnk/sharedBackpackKt/inventory/CustomFurnaceMenu.kt
@@ -0,0 +1,43 @@
+package io.github.ykysnk.sharedBackpackKt.inventory
+
+import net.minecraft.world.Container
+import net.minecraft.world.entity.player.Inventory
+import net.minecraft.world.entity.player.Player
+import net.minecraft.world.inventory.AbstractFurnaceMenu
+import net.minecraft.world.inventory.ContainerData
+import net.minecraft.world.inventory.MenuType
+import net.minecraft.world.inventory.RecipeBookType
+import net.minecraft.world.item.crafting.RecipeType
+
+@Suppress("unused")
+class CustomFurnaceMenu : AbstractFurnaceMenu {
+    constructor(containerId: Int, playerInventory: Inventory) : super(
+        MenuType.FURNACE,
+        RecipeType.SMELTING,
+        RecipeBookType.FURNACE,
+        containerId,
+        playerInventory
+    )
+
+    constructor(
+        containerId: Int,
+        playerInventory: Inventory,
+        furnaceContainer: Container?,
+        furnaceData: ContainerData?
+    ) : super(
+        MenuType.FURNACE,
+        RecipeType.SMELTING,
+        RecipeBookType.FURNACE,
+        containerId,
+        playerInventory,
+        furnaceContainer,
+        furnaceData
+    ) {
+        furnaceContainer?.startOpen(playerInventory.player)
+    }
+
+    override fun removed(player: Player) {
+        super.removed(player)
+        container.stopOpen(player)
+    }
+}
