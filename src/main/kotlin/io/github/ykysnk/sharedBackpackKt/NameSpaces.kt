36511cb9164f2db603a91e9845c724e3278c6b0f
yukiyosanako <179217493+T2PeNBiX99wcoxKv3A4g@users.noreply.github.com>
2026-09-13 17:22:59 +0800

    refactor(core): add `Constants` and `NameSpaces` for improved modularity and configuration handling

---
+++ src/main/kotlin/io/github/ykysnk/sharedBackpackKt/NameSpaces.kt
@@ -0,0 +1,15 @@
+package io.github.ykysnk.sharedBackpackKt
+
+import net.minecraft.resources.ResourceLocation
+
+enum class NameSpaces(val id: String) {
+    MOD("shared-backpack-kt"),
+    FORGE("c"),
+    MINECRAFT("minecraft");
+
+    fun path(path: String) = ResourceLocation(id, path)
+
+    override fun toString(): String = id
+    operator fun invoke() = id
+    operator fun invoke(path: String) = path(path)
+}
