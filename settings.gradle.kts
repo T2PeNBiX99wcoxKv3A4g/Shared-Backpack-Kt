5ff54fd94374f1e06a93d1afaea8a795c0e0cba4
yukiyosanako <179217493+T2PeNBiX99wcoxKv3A4g@users.noreply.github.com>
2026-09-13 16:14:39 +0800

    refactor(build): update Gradle configuration for improved modularity and dependency management

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,9 +1,18 @@
 pluginManagement {
     repositories {
-        mavenCentral()
         maven("https://maven.fabricmc.net/") {
             name = "Fabric"
         }
+        mavenCentral()
         gradlePluginPortal()
     }
+
+    plugins {
+        id("net.fabricmc.fabric-loom-remap") version providers.gradleProperty("loom_version")
+        kotlin("jvm") version providers.gradleProperty("jvm_version")
+        kotlin("plugin.serialization") version providers.gradleProperty("jvm_version")
+    }
 }
+
+// Should match your modid
+rootProject.name = "shared-backpack-kt"
