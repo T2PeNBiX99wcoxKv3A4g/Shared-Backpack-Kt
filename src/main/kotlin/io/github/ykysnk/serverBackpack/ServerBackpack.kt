3f16498849c9db92899dbb111fecae3a4d7694c9
yukiyosanako <179217493+T2PeNBiX99wcoxKv3A4g@users.noreply.github.com>
2026-09-13 20:51:42 +0800

    refactor(namespace, lang): rename mod from `shared-backpack-kt` to `server-backpack` across files

--- src/main/kotlin/io/github/ykysnk/serverBackpack/ServerBackpack.kt
+++ src/main/kotlin/io/github/ykysnk/serverBackpack/ServerBackpack.kt
@@ -3,7 +3,7 @@ package io.github.ykysnk.serverBackpack
 import io.github.ykysnk.serverBackpack.Constants.ForceInitialize
 import net.fabricmc.api.ModInitializer
 
-object SharedBackpackKt : ModInitializer {
+object ServerBackpack : ModInitializer {
     override fun onInitialize() {
         ForceInitialize
     }
